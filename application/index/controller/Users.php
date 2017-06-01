a09b7a77a21b3a1673dba3daacc02cad7a572d0e
hequanli <guoqingzhe@hotmail.com>
2017-05-31 18:29:21 -0700

    用户增删改查，登陆基本完成

--- application/index/controller/Users.php
+++ application/index/controller/Users.php
@@ -9,8 +9,9 @@
 namespace app\index\controller;
 use think\Controller;
 use think\Request;
-use app\index\model\UserModel;
+use think\Session;
 
+use app\index\model\UserModel;
 class Users extends Controller
 {
     public function user_list(){
@@ -19,10 +20,83 @@ class Users extends Controller
         //header('Access-Control-Allow-Headers : token,accept,content-type,X-Requested-With');
 
         //$post_data  = Request::instance()->post();
+        if(!session('user_username')){
+            return json(['status'=>0,'msg'=>'请先登录']);
+        }
         $User = new UserModel();
         $data = $User -> showlist(0);
-
-
         return json($data);
+    }
+    public function user_del(){
+        //header('Access-Control-Allow-Origin : *');//有的Apache可能需要关掉header才能进行线下localhost调试
+        //header('Access-Control-Allow-Methods : POST,GET,PUT,DELETE,OPTIONS');
+        //header('Access-Control-Allow-Headers : token,accept,content-type,X-Requested-With');
+        if(!session('user_username')){
+            return ['status'=>0,'msg'=>'请先登录'];
+        }
+        $User = new UserModel();
+        $user_id = Request::instance()->post('id');
+        //var_dump($user_id);
+        $res = $User -> del_user($user_id);
+        //var_dump($res);
+        if(!$res){
+            return json(['status'=>0,'msg'=>'参数错误']);
+        }else{
+            return json(['status'=>1,'msg'=>'删除用户成功','id'=>$user_id]);
+        }
+    }
+    public function user_add(){
+        //header('Access-Control-Allow-Origin : *');//有的Apache可能需要关掉header才能进行线下localhost调试
+        //header('Access-Control-Allow-Methods : POST,GET,PUT,DELETE,OPTIONS');
+        //header('Access-Control-Allow-Headers : token,accept,content-type,X-Requested-With');
+        $User = new UserModel();
+        $data = Request::instance()->post();
+
+        $res = $User -> adduser($data);
+        if($res){
+            return json(['status'=>1,'msg'=>'用户添加成功']);
+        }else{
+            return json(['status'=>0,'msg'=>'用户添加失败']);
+        }
+    }
+    public function user_showdetail(){
+        //header('Access-Control-Allow-Origin : *');//有的Apache可能需要关掉header才能进行线下localhost调试
+        //header('Access-Control-Allow-Methods : POST,GET,PUT,DELETE,OPTIONS');
+        //header('Access-Control-Allow-Headers : token,accept,content-type,X-Requested-With');
+
+        $User = new UserModel();
+        $user_id = Request::instance()->post('id');
+
+        $res = $User -> userdetail($user_id);
+        if($res){
+            return json(['status'=>1,'data'=>$res]);
+        }else{
+            return json(['status'=>0,'msg'=>'查询信息失败']);
+        }
+    }
+    public function user_login(){
+        //header('Access-Control-Allow-Origin : *');//有的Apache可能需要关掉header才能进行线下localhost调试
+        //header('Access-Control-Allow-Methods : POST,GET,PUT,DELETE,OPTIONS');
+        //header('Access-Control-Allow-Headers : token,accept,content-type,X-Requested-With');
+
+        $User = new UserModel();
+        $user_name = Request::instance()->get('username');
+        $user_password = Request::instance()->get('password');
+        $res = $User -> user_loginer($user_name,$user_password);
+        if($res){
+            //session写入
+            session::clear();
+            Session::set('user_username', $user_name);
+            Session::set('user_id', $res[0]['id']);
+            Session::set('user_group', $res[0]['group']);
+            return json(['status'=>1,'msg'=>'登陆成功']);
+        }else{
+            session::clear();
+            return json(['status'=>0,'msg'=>'登陆失败，用户名或者密码错误']);
+        }
+    }
+    public function user_logout(){
+        session::clear();
+        return json(['status'=>1,'msg'=>'用户注销成功']);
     }
 }
--- application/index/model/UserModel.php
+++ application/index/model/UserModel.php
@@ -9,8 +9,10 @@
 
 namespace app\index\model;
 
+use phpDocumentor\Reflection\Types\Null_;
 use think\Model;
 use think\Db;
+use think\Session;
 class UserModel
 {
     public function showlist($cate = '0'){ //cate代表查询的usergroup 0则为全部查询，默认为0
@@ -25,4 +27,52 @@ class UserModel
             return $db;
         }
     }
+    public function del_user($id = '0'){
+        if($id == '0'){
+            return false;
+        }else{
+            $db = Db::name('user')
+                ->where('id','=',$id)
+                ->delete();
+            return $db;
+        }
+    }
+    public function adduser($data){
+        if($data['username']==NULL || $data['password']==NULL || $data['usergroup']==NULL){
+            return false;
+        }else{
+            $data['password'] = md5($data['password']);
+            $data['userstatus'] = 1;
+            $db = Db::name('user')
+                ->insert($data);
+            return $db;
+        }
+    }
+    public function userdetail($id='0'){
+        if($id == '0'){
+            return false;
+        }else{
+            $db = Db('user')
+                ->where('id','=',$id)
+                ->select();
+            return $db;
+        }
+    }
+    public function user_loginer($username,$password){
+        if($username==NULL || $password==NULL){
+            return false;
+        }else{
+            $password = md5($password);
+            $username = Db::name('user')
+                ->where('username','=',$username)
+                ->select();
+            if($username[0]['password'] == $password){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+    }
+
 }
